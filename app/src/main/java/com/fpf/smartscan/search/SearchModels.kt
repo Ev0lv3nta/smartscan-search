46c439aeddb486c0cdf7c6b69555d86286b967db
d41dev <d41dev@proton.me>
2025-12-15 20:36:06 +0000

    update: add support for video search from external apps via intents

--- app/src/main/java/com/fpf/smartscan/search/SearchModels.kt
+++ app/src/main/java/com/fpf/smartscan/search/SearchModels.kt
@@ -1,6 +1,7 @@
 package com.fpf.smartscan.search
 
 import android.net.Uri
+import com.fpf.smartscan.media.MediaType
 
 
 enum class QueryType {
@@ -14,6 +15,6 @@ enum class SearchMode {
 enum class ProcessorStatus {IDLE, ACTIVE, COMPLETE, FAILED }
 
 sealed interface SearchQuery{
-    data class ImageQuery(val uri: Uri): SearchQuery
-    data class TextQuery(val text: String): SearchQuery
+    data class ImageQuery(val uri: Uri, val mediaType: MediaType): SearchQuery
+    data class TextQuery(val text: String, val mediaType: MediaType): SearchQuery
 }
