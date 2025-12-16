c5f71071a4fcd99c2920874a47d7403431e87c43
d41dev <d41dev@proton.me>
2025-12-16 19:21:50 +0000

    fix: add foregin keys and index

--- app/src/main/java/com/fpf/smartscan/data/videos/VideoTagCrossRefEntity.kt
+++ app/src/main/java/com/fpf/smartscan/data/videos/VideoTagCrossRefEntity.kt
@@ -1,10 +1,21 @@
 package com.fpf.smartscan.data.videos
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 
 @Entity(
     tableName = "video_tag_crossref",
-    primaryKeys = ["videoId", "tag"]
+    primaryKeys = ["videoId", "tag"],
+    foreignKeys = [
+        ForeignKey(
+            entity = VideoTag::class,
+            parentColumns = ["name"],
+            childColumns = ["tag"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index("tag")]
 )
 data class VideoTagCrossRef(
     val videoId: Long,
