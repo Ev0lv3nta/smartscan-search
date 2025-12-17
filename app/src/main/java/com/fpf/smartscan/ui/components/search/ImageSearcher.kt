b7efe7862331e81d6799f3168b0e630e1c4262f7
d41dev <d41dev@proton.me>
2025-12-17 23:14:28 +0000

    update: add guard in imagesearcher to prevent uncessary searches when navigating to and from search screen

--- app/src/main/java/com/fpf/smartscan/ui/components/search/ImageSearcher.kt
+++ app/src/main/java/com/fpf/smartscan/ui/components/search/ImageSearcher.kt
@@ -24,6 +24,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -52,8 +56,12 @@ fun ImageSearcher(
 ){
     if(uri == null) return
 
+    var currentMediaType by remember { mutableStateOf(mediaType) }
+
     LaunchedEffect(mediaType) {
+        if(currentMediaType == mediaType) return@LaunchedEffect
         onSearch()
+        currentMediaType = mediaType
     }
 
     Row (
