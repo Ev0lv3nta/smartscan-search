a459ab47db7e491a5e15cd59c825c9fd49d2c86a
d41dev <d41dev@proton.me>
2025-12-17 00:43:34 +0000

    update imagesearch design

--- app/src/main/java/com/fpf/smartscan/ui/components/search/ImageSearcher.kt
+++ app/src/main/java/com/fpf/smartscan/ui/components/search/ImageSearcher.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -50,13 +51,10 @@ fun ImageSearcher(
         onSearch()
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(4.dp),
-
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+    Row (
+        modifier = Modifier.fillMaxWidth().padding(4.dp),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Box(
             modifier = Modifier
