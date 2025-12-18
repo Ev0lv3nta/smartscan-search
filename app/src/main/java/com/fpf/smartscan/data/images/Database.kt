5315401eeae0416d4b3a183393d945e0e4ba9de5
d41dev <d41dev@proton.me>
2025-12-18 02:23:36 +0000

    update: add logic to restore db

--- app/src/main/java/com/fpf/smartscan/data/images/Database.kt
+++ app/src/main/java/com/fpf/smartscan/data/images/Database.kt
@@ -1,9 +1,11 @@
 package com.fpf.smartscan.data.images
 
 import android.app.Application
+import android.util.Log
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import java.io.File
 
 @Database(entities = [ImageTag::class, ImageTagCrossRef::class], version = 1, exportSchema = false)
 abstract class ImageTagDatabase : RoomDatabase() {
@@ -15,12 +17,22 @@ abstract class ImageTagDatabase : RoomDatabase() {
         private var INSTANCE: ImageTagDatabase? = null
 
         fun getDatabase(application: Application): ImageTagDatabase {
+            val dbName = "image_tag_database"
+            val dbFile = File(application.filesDir, dbName)
+            val dbPath = application.getDatabasePath(dbName)
+
+            if (dbFile.exists()) {
+                Log.d("ImageTagDatabase", "Database cache found, restoring...")
+                dbFile.copyTo(dbPath, overwrite = true)
+                dbFile.delete()
+            }
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     application,
                     ImageTagDatabase::class.java,
-                    "image_tag_database"
-                ).build()
+                    dbName
+                ).setJournalMode(JournalMode.TRUNCATE)
+                    .build()
                 INSTANCE = instance
                 instance
             }
--- app/src/main/java/com/fpf/smartscan/data/videos/Database.kt
+++ app/src/main/java/com/fpf/smartscan/data/videos/Database.kt
@@ -1,9 +1,11 @@
 package com.fpf.smartscan.data.videos
 
 import android.app.Application
+import android.util.Log
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import java.io.File
 
 
 @Database(entities = [VideoTag::class, VideoTagCrossRef::class], version = 1, exportSchema = false)
@@ -15,12 +17,21 @@ abstract class VideoTagDatabase : RoomDatabase() {
         private var INSTANCE: VideoTagDatabase? = null
 
         fun getDatabase(application: Application): VideoTagDatabase {
+            val dbName = "video_tag_database"
+            val dbFile = File(application.filesDir, dbName)
+            val dbPath = application.getDatabasePath(dbName)
+
+            if (dbFile.exists()) {
+                Log.d("VideoTagDatabase", "Database cache found, restoring...")
+                dbFile.copyTo(dbPath, overwrite = true)
+                dbFile.delete()
+            }
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     application,
                     VideoTagDatabase::class.java,
-                    "video_tag_database"
-                ).build()
+                    dbName
+                ).setJournalMode(JournalMode.TRUNCATE).build()
                 INSTANCE = instance
                 instance
             }
