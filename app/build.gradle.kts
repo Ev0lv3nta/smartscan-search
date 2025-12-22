825c9073a70d7b80b77ccc1523d5273234bfcdbc
d41dev <d41dev@proton.me>
2025-12-22 17:56:56 +0000

    update: bump sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,12 +80,12 @@ android {
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.documentfile)
+    implementation(libs.smartscan.ml)
 
+    // media loading
     implementation(libs.coil.compose)
     implementation(libs.coil.video)
-
-    implementation(libs.smartscan.sdk.smartscan.ml)
-
+    
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
