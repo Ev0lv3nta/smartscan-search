4e1ffc7081b8098ad74657348772adac3c5300e3
d41dev <d41dev@proton.me>
2025-12-27 22:13:43 +0000

    update: show play icon for videos

--- app/src/main/java/com/fpf/smartscan/ui/components/media/ImageDisplay.kt
+++ app/src/main/java/com/fpf/smartscan/ui/components/media/ImageDisplay.kt
@@ -4,23 +4,30 @@ import android.net.Uri
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.PlayCircle
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.zIndex
 import coil3.compose.AsyncImage
 import coil3.request.ImageRequest
 import coil3.request.allowHardware
 import coil3.request.crossfade
+import com.fpf.smartscan.media.MediaType
 
 @Composable
 fun ImageDisplay(
     uri: Uri,
     modifier: Modifier = Modifier,
     contentScale: ContentScale = ContentScale.Crop,
-    maxSize:Int = 512
+    maxSize:Int = 512,
+    mediaType: MediaType,
 ) {
     val context = LocalContext.current
 
@@ -32,6 +39,10 @@ fun ImageDisplay(
         .build()
 
     Box(modifier = modifier.background(Color.Transparent), contentAlignment = Alignment.Center) {
+        if(mediaType == MediaType.VIDEO){
+            Icon(Icons.Filled.PlayCircle, contentDescription = null, modifier = Modifier.align(
+                Alignment.Center).zIndex(1f))
+        }
         AsyncImage(
             model = request,
             contentDescription = "Displayed image",
