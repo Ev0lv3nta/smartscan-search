7d1edb5d1ff56d906e7c2953271080aaed8e8741
d41dev <d41dev@proton.me>
2026-04-07 10:29:00 +0100

    update: add image and video cluster embedding store filenames

--- app/src/main/java/com/fpf/smartscan/constants/Search.kt
+++ app/src/main/java/com/fpf/smartscan/constants/Search.kt
@@ -8,13 +8,12 @@ val mediaTypeOptions = mapOf(
     MediaType.VIDEO to "Videos",
 )
 
-val queryOptions = mapOf(
-    QueryType.TEXT to "Text",
-    QueryType.IMAGE to "Image"
-)
-
 object EmbeddingStoresFiles{
     const val IMAGE: String = "image_index.bin"
     const val VIDEO: String = "video_index.bin"
+
+    const val IMAGE_CLUSTER: String = "image_cluster_index.bin"
+
+    const val VIDEO_CLUSTER: String = "video_cluster_index.bin"
     const val TAGS: String = "tags_store.bin"
 }
