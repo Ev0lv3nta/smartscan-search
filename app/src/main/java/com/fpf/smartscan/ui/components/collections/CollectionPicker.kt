9e96152c1085ce10fde1d9fbf10635728d763859
d41dev <d41dev@proton.me>
2026-04-13 02:29:10 +0100

    update: add collection picker

---
+++ app/src/main/java/com/fpf/smartscan/ui/components/collections/CollectionPicker.kt
@@ -0,0 +1,80 @@
+package com.fpf.smartscan.ui.components.collections
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Text
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
+import com.fpf.smartscan.collections.MediaCollection
+import com.fpf.smartscan.media.MediaType
+
+@Composable
+fun CollectionPicker(
+    collections: List<MediaCollection>,
+    mediaType: MediaType,
+    onClose: () -> Unit,
+    onSelectCollection: (collection: MediaCollection) -> Unit,
+) {
+    Popup(
+        onDismissRequest = { onClose() },
+        properties = PopupProperties(
+            dismissOnBackPress = true,
+            focusable = true
+        )
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            Column {
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(4.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    IconButton(onClick = onClose) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onBackground
+                        )
+                    }
+
+                    Text(
+                        text = "Pick collection",
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 8.dp)
+                ) {
+                    MediaCollectionsList(
+                        isVisible = true,
+                        numGridColumns = 3,
+                        mediaType = mediaType,
+                        items = collections,
+                        onItemClick = onSelectCollection,
+                    )
+                }
+            }
+        }
+    }
+}
