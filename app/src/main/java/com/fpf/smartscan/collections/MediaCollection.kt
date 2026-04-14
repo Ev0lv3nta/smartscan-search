5fe7b0b411682f451beb070382209fdd2358bece
d41dev <d41dev@proton.me>
2026-04-14 19:28:24 +0100

    update: add id and isAutoCollection

--- app/src/main/java/com/fpf/smartscan/collections/MediaCollection.kt
+++ app/src/main/java/com/fpf/smartscan/collections/MediaCollection.kt
@@ -3,7 +3,9 @@ package com.fpf.smartscan.collections
 import android.net.Uri
 
 data class MediaCollection (
+    val id: Long,
     val name: String,
     val thumbNail: Uri,
     val size: Int,
+    val isAutoCollection: Boolean = false
 )
