18d6034a7d3bb11c24ed56af0b3ed2a61d20fb7a
d41dev <d41dev@proton.me>
2026-04-15 00:13:16 +0100

    update: create cluster paging source

---
+++ app/src/main/java/com/fpf/smartscan/data/clusters/ClusterPagingSource.kt
@@ -0,0 +1,47 @@
+package com.fpf.smartscan.data.clusters
+
+import android.net.Uri
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.fpf.smartscan.media.MediaType
+
+class ClusterPagingSource(
+    private val mediaType: MediaType,
+    private val clusterId: Long,
+    private val clusterCrossRefRepository: ClusterCrossRefRepository,
+    private val mediaIdToUri: (Long, MediaType) -> Uri
+) : PagingSource<Int, Uri>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Uri> {
+        val page = params.key ?: 0
+        val pageSize = params.loadSize
+        val offset = page * pageSize
+
+        // over-fetch by 1 item to detect end of data without using count()
+        return try {
+            val ids = clusterCrossRefRepository.getMediaIds(clusterId = clusterId, limit = pageSize + 1, offset = offset)
+            val hasMore = ids.size > pageSize
+            val pageItems = if (hasMore) ids.dropLast(1) else ids
+
+            val uris = pageItems.map { id ->
+                mediaIdToUri(id, mediaType)
+            }
+
+            LoadResult.Page(
+                data = uris,
+                prevKey = if (page == 0) null else page - 1,
+                nextKey = if (hasMore) page + 1 else null
+            )
+
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Uri>): Int? {
+        return state.anchorPosition?.let { pos ->
+            state.closestPageToPosition(pos)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(pos)?.nextKey?.minus(1)
+        }
+    }
+}
