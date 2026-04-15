362a77ad684681dde8fb6fa6fb98d1b8e3effc9d
d41dev <d41dev@proton.me>
2026-04-15 11:51:09 +0100

    refactor: support optional filtering my mediaType

--- app/src/main/java/com/fpf/smartscan/data/clusters/ClusterPagingSource.kt
+++ app/src/main/java/com/fpf/smartscan/data/clusters/ClusterPagingSource.kt
@@ -6,7 +6,7 @@ import androidx.paging.PagingState
 import com.fpf.smartscan.media.MediaType
 
 class ClusterPagingSource(
-    private val mediaType: MediaType,
+    private val mediaType: MediaType? = null,
     private val clusterId: Long,
     private val clusterCrossRefRepository: ClusterCrossRefRepository,
     private val mediaIdToUri: (Long, MediaType) -> Uri
@@ -19,12 +19,16 @@ class ClusterPagingSource(
 
         // over-fetch by 1 item to detect end of data without using count()
         return try {
-            val ids = clusterCrossRefRepository.getMediaIds(clusterId = clusterId, limit = pageSize + 1, offset = offset)
-            val hasMore = ids.size > pageSize
-            val pageItems = if (hasMore) ids.dropLast(1) else ids
+            val crossRefs = if(mediaType != null){
+                clusterCrossRefRepository.getByClusterIdAndType(clusterId = clusterId, mediaType, limit = pageSize + 1, offset = offset)
+            }else{
+                clusterCrossRefRepository.getByClusterId(clusterId = clusterId, limit = pageSize + 1, offset = offset)
+            }
+            val hasMore = crossRefs.size > pageSize
+            val pageItems = if (hasMore) crossRefs.dropLast(1) else crossRefs
 
-            val uris = pageItems.map { id ->
-                mediaIdToUri(id, mediaType)
+            val uris = pageItems.map { crossRef ->
+                mediaIdToUri(crossRef.clusterId, crossRef.type)
             }
 
             LoadResult.Page(
