bebd634d46186d6e4db158a92b64762be3ed24b3
d41dev <d41dev@proton.me>
2026-04-17 10:39:14 +0100

    add and use MediaItemCard in CollectionItemsList

--- app/src/main/java/com/fpf/smartscan/ui/components/collections/CollectionItemsList.kt
+++ app/src/main/java/com/fpf/smartscan/ui/components/collections/CollectionItemsList.kt
@@ -4,14 +4,9 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.border
-import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -32,17 +27,14 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
 import coil3.compose.AsyncImagePainter
 import com.fpf.smartscan.media.MediaItem
-import com.fpf.smartscan.ui.components.CircularCheckbox
-import com.fpf.smartscan.ui.components.media.ImageDisplay
+import com.fpf.smartscan.media.MediaItemCard
 import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
@@ -126,47 +118,15 @@ fun CollectionItemsList(
                 val item = items[index]
 
                 if (item != null) {
-                    Box(
-                        modifier = Modifier
-                            .aspectRatio(1f)
-                            .padding(4.dp)
-                            .border(1.dp, Color.Gray.copy(alpha = 0.2f))
-                            .combinedClickable(
-                                indication = null,
-                                interactionSource = remember { MutableInteractionSource() },
-                                onClick = {
-                                    if (isSelecting) {
-                                        onToggleSelected(item)
-                                    } else {
-                                        onViewItem(item)
-                                    }
-                                },
-                                onLongClick = {
-                                    if (!isSelecting) {
-                                        onToggleSelectionMode()
-                                        onToggleSelected(item)
-                                    }
-                                }
-                            )
-                    ) {
-                        ImageDisplay(
-                            uri = item.uri,
-                            modifier = Modifier.fillMaxSize(),
-                            contentScale = ContentScale.Crop,
-                            mediaType = item.type,
-                            onError=onError
-                        )
-
-                        if (isSelecting) {
-                            CircularCheckbox(
-                                checked = item in selectedItems,
-                                onCheckedChange = { onToggleSelected(item) },
-                                modifier = Modifier
-                                    .offset(x = 8.dp, y = 8.dp)
-                                    .align(Alignment.TopStart),
-                            )
-                        }
-                    }
+                    MediaItemCard(
+                        item=item,
+                        onViewItem=onViewItem,
+                        onToggleSelected = onToggleSelected,
+                        onToggleSelectionMode = onToggleSelectionMode,
+                        isSelecting = isSelecting,
+                        isChecked = { item in selectedItems },
+                        onError=onError
+                    )
                 }
             }
         }
@@ -191,3 +151,4 @@ fun CollectionItemsList(
         }
     }
 }
+
