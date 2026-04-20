847614516cf4ab0a9bb0edbefecbe7446eb5b703
d41dev <d41dev@proton.me>
2026-04-20 11:55:22 +0100

    update: remove mavenLocal

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,9 +15,8 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        mavenLocal()
         mavenCentral()
-//        maven(url = "https://jitpack.io")
+        maven(url = "https://jitpack.io")
     }
 }
 
