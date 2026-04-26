975395108606b8a5b4e50f2cfe3042ea1873f617
d41dev <d41dev@proton.me>
2026-04-26 14:16:22 +0100

    update: add prefs constants

---
+++ app/src/main/java/com/fpf/smartscan/constants/Prefs.kt
@@ -0,0 +1,9 @@
+package com.fpf.smartscan.constants
+
+object PrefsNames {
+    const val APP_PREFS = "AsyncStorage" // for backward compatibility
+}
+
+object PrefsKeys {
+    const val KEY_SYNC_COMPLETE = "sync_complete"
+}
