6cf4b9dbd356ec5a477d3f3801bc1636e2957737
d41dev <d41dev@proton.me>
2026-04-27 01:52:26 +0100

    update: helper for embedding store sync

---
+++ app/src/main/java/com/fpf/smartscan/data/EmbedStoreSyncHelper.kt
@@ -0,0 +1,79 @@
+package com.fpf.smartscan.data
+
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.util.Log
+import androidx.core.content.edit
+import com.fpf.smartscan.constants.EmbeddingStoresFiles
+import com.fpf.smartscan.constants.PrefsKeys
+import com.fpf.smartscan.constants.PrefsNames
+import com.fpf.smartscan.media.getImageToDateMap
+import com.fpf.smartscan.media.getVideoToDateMap
+import com.fpf.smartscan.search.ImageIndexListener
+import com.fpf.smartscan.search.VideoIndexListener
+import com.fpf.smartscansdk.core.embeddings.FileEmbeddingStore
+import com.fpf.smartscansdk.core.processors.Metrics
+import java.io.File
+import kotlin.collections.map
+import kotlin.collections.mapNotNull
+
+object EmbedStoreSyncHelper {
+    private const val TAG = "EmbedStoreSyncHelper"
+    private const val EMBED_DIM: Int = 512
+
+    suspend fun syncStores(
+        context: Context,
+        imageStore: FileEmbeddingStore,
+        videoStore: FileEmbeddingStore
+    ) {
+        val sharedPrefs = context.applicationContext.getSharedPreferences(PrefsNames.APP_PREFS, MODE_PRIVATE)
+
+        syncStoreDates(
+            context=context,
+            store = imageStore,
+            tempFileName = "${EmbeddingStoresFiles.IMAGE}.tmp",
+            finalFileName = EmbeddingStoresFiles.IMAGE,
+            idToDate = { ids -> getImageToDateMap(context.applicationContext, ids) }
+        )
+
+        syncStoreDates(
+            context=context,
+            store = videoStore,
+            tempFileName = "${EmbeddingStoresFiles.VIDEO}.tmp",
+            finalFileName = EmbeddingStoresFiles.VIDEO,
+            idToDate = { ids -> getVideoToDateMap(context.applicationContext, ids) }
+        )
+        sharedPrefs.edit {
+            putBoolean(PrefsKeys.SYNC_COMPLETE, true)
+        }
+        Log.d(TAG, "Sync complete successfully")
+        // force trigger refresh on index
+        ImageIndexListener.onComplete(context.applicationContext, Metrics.Success())
+        VideoIndexListener.onComplete(context.applicationContext, Metrics.Success())
+    }
+
+    private suspend fun syncStoreDates(
+        context: Context,
+        store: FileEmbeddingStore,
+        tempFileName: String,
+        finalFileName: String,
+        idToDate: suspend (List<Long>) -> Map<Long, Long>,
+    ) {
+        if (!store.exists) return
+        val embeds = store.get()
+        val dateMap = idToDate(embeds.map { it.id })
+        val updated = embeds.mapNotNull {
+            val date = dateMap[it.id] ?: return@mapNotNull null
+            it.copy(date = date)
+        }
+
+        val tempFile = File(context.applicationContext.cacheDir, tempFileName)
+        val tempStore = FileEmbeddingStore(tempFile, EMBED_DIM)
+        tempStore.add(updated)
+
+        val finalFile = File(context.applicationContext.filesDir, finalFileName)
+        if (finalFile.exists()) finalFile.delete()
+        tempFile.renameTo(finalFile)
+    }
+
+}
