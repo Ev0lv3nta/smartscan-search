7fe77a9aa9e572d71bdb26d2c9a8766a404cfc27
d41dev <d41dev@proton.me>
2026-04-29 02:44:20 +0100

    update: add method to rebuildIndex

--- app/src/main/java/com/fpf/smartscan/services/IndexServiceManager.kt
+++ app/src/main/java/com/fpf/smartscan/services/IndexServiceManager.kt
@@ -2,8 +2,10 @@ package com.fpf.smartscan.services
 
 import android.content.Context
 import android.content.Intent
+import com.fpf.smartscan.constants.EmbeddingStoresFiles
 import com.fpf.smartscan.media.MediaType
 import com.fpf.smartscan.utils.isServiceRunning
+import java.io.File
 
 fun startIndexing(context: Context, mediaTypes: List<MediaType>) {
     Intent(context.applicationContext, MediaIndexForegroundService::class.java)
@@ -15,6 +17,26 @@ fun startIndexing(context: Context, mediaTypes: List<MediaType>) {
 }
 
 fun refreshIndex(context: Context, mediaTypes: List<MediaType>) {
+    val running = isServiceRunning(context.applicationContext, MediaIndexForegroundService::class.java)
+    if(running){
+        context.applicationContext.stopService(Intent(context.applicationContext, MediaIndexForegroundService::class.java))
+    }
+    startIndexing(context.applicationContext, mediaTypes)
+}
+
+fun rebuildIndex(context: Context, mediaTypes: List<MediaType>) {
+    mediaTypes.forEach { type ->
+        when(type){
+            MediaType.IMAGE -> {
+                File(context.filesDir, EmbeddingStoresFiles.IMAGE).delete()
+                File(context.filesDir, EmbeddingStoresFiles.IMAGE_CLUSTER).delete()
+            }
+            MediaType.VIDEO -> {
+                File(context.filesDir, EmbeddingStoresFiles.VIDEO).delete()
+                File(context.filesDir, EmbeddingStoresFiles.VIDEO_CLUSTER).delete()
+            }
+        }
+    }
     val running = isServiceRunning(context.applicationContext, MediaIndexForegroundService::class.java)
     if(running){
         context.applicationContext.stopService(Intent(context.applicationContext, MediaIndexForegroundService::class.java))
