644dc7938d9b0bab603dfc0d1c9e2b1b45e607ef
d41dev <d41dev@proton.me>
2026-05-12 16:28:50 +0100

    update: add loading indicator

--- app/src/main/java/com/fpf/smartscan/ui/components/modals/TextInputModal.kt
+++ app/src/main/java/com/fpf/smartscan/ui/components/modals/TextInputModal.kt
@@ -1,6 +1,8 @@
 package com.fpf.smartscan.ui.components.modals
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -11,14 +13,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import com.fpf.smartscan.ui.components.LoadingIndicator
+import com.google.android.material.carousel.Arrangement
 import kotlinx.coroutines.FlowPreview
 
 
 @OptIn(FlowPreview::class)
 @Composable
 fun TextInputModal(
+    isLoading: Boolean? = null,
     isVisible: Boolean,
     title: String,
     onClose: () -> Unit,
@@ -53,14 +61,28 @@ fun TextInputModal(
             }
         },
         dismissButton = {
-            TextButton(onClick = onClose) { Text("Cancel") }
+            TextButton(
+                enabled = isLoading != true,
+                onClick = onClose) { Text("Cancel") }
         },
         confirmButton = {
-            TextButton(onClick = {
-                onConfirm(value.text)
-                onClose()
-            }) {
-                Text("Confirm")
+            Row(
+                verticalAlignment = Alignment.CenterVertically) {
+                TextButton(
+                    enabled = isLoading != true,
+                    onClick = {
+                    onConfirm(value.text)
+                }) {
+                    Text("Confirm")
+                }
+                if(isLoading == true){
+                    LoadingIndicator(
+                        isVisible = true,
+                        size = 18.dp,
+                        strokeWidth = 2.dp,
+                        modifier = Modifier.padding(start = 6.dp)
+                    )
+                }
             }
         }
     )
