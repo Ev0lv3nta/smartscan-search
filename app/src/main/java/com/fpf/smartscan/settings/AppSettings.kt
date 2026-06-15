0723bfcba2e2e5d325e8ca11fb4a273f324fc86a
d41dev <d41dev@proton.me>
2026-06-15 01:09:55 +0100

    refactor: set default strictness to 0.4

--- app/src/main/java/com/fpf/smartscan/settings/AppSettings.kt
+++ app/src/main/java/com/fpf/smartscan/settings/AppSettings.kt
@@ -6,8 +6,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class AppSettings(
-    val textQueryStrictness: Float = 0f,
-    val imageQueryStrictness: Float = 0f,
+    val textQueryStrictness: Float = 0.4f,
+    val imageQueryStrictness: Float = 0.4f,
     val searchableImageDirectories: List<String> = emptyList(),
     val searchableVideoDirectories: List<String> = emptyList(),
     val theme: ThemeMode = ThemeMode.SYSTEM,
