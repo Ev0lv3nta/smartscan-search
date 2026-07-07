c3ed4b779ea93d8ab5d2d45c8cf4e40a988d58f2
d41dev <d41dev@proton.me>
2026-07-07 20:27:33 +0100

    update: removed support for old data

--- app/src/main/java/com/fpf/smartscan/data/old/images/ImageTagRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.fpf.smartscan.data.old.images
-
-class ImageTagRepository(private val dao: ImageTagDao) {
-    suspend fun getAllTags(): List<ImageTag> = dao.getAll()
-}
--- app/src/main/java/com/fpf/smartscan/data/old/videos/VideoTagCrossRefRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.fpf.smartscan.data.old.videos
-
-class VideoTagCrossRefRepository(
-    private val dao: VideoTagCrossRefDao,
-) {
-    suspend fun getAllCrossRefs(): List<VideoTagCrossRef> = dao.getAll()
-}
